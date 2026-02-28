0c65453e66ea6de96ca89ff4907be180164bd9f9
davci-udel <davci@udel.edu>
2026-02-27 23:51:02 -0500

    Implement L3 parse and lark rules so all tests pass

--- packages/L3/src/L3/parse.py
+++ packages/L3/src/L3/parse.py
@@ -1,13 +1,26 @@
 from collections.abc import Sequence
 from pathlib import Path
+from typing import cast
 
-from lark import Lark, Token, Transformer
+from lark import Lark, Token, Transformer, Tree
 from lark.visitors import v_args  # pyright: ignore[reportUnknownVariableType]
 
 from .syntax import (
+    Abstract,
+    Allocate,
+    Apply,
+    Begin,
+    Branch,
     Identifier,
+    Immediate,
     Let,
+    LetRec,
+    Load,
+    Nat,
+    Primitive,
     Program,
+    Reference,
+    Store,
     Term,
 )
 
@@ -25,6 +38,30 @@ class AstTransformer(Transformer[Token, Program | Term]):
             body=body,
         )
 
+    def __default_token__(
+        self,
+        wild: Token,
+    ) -> Token | Identifier | Immediate:
+        if wild.type == "IDENTIFIER":
+            return wild.value
+        if wild.type == "INTEGER":
+            return Immediate(value=int(wild.value))
+        return wild
+
+    @v_args(inline=True)
+    def number(
+        self,
+        wild: Token,
+    ) -> Immediate:
+        return Immediate(value=int(wild.value))
+
+    @v_args(inline=True)
+    def nat(
+        self,
+        wild: Token,
+    ) -> Nat:
+        return int(wild.value)
+
     def parameters(
         self,
         parameters: Sequence[Identifier],
@@ -36,6 +73,8 @@ class AstTransformer(Transformer[Token, Program | Term]):
         self,
         term: Term,
     ) -> Term:
+        if isinstance(term, str):
+            return Reference(name=term)
         return term
 
     @v_args(inline=True)
@@ -57,10 +96,58 @@ class AstTransformer(Transformer[Token, Program | Term]):
         bindings: Sequence[tuple[Identifier, Term]],
         body: Term,
     ) -> Term:
-        return Let(
+        return LetRec(
             bindings=bindings,
             body=body,
         )
+
+    @v_args(inline=True)
+    def reference(
+        self,
+        _: Token,
+        name: Identifier,
+    ) -> Term:
+        return Reference(name=name)
+
+    @v_args(inline=True)
+    def abstract(self, _: Token, parameters: Sequence[Identifier], body: Term) -> Term:
+        return Abstract(parameters=parameters, body=body)
+
+    def apply(self, body: list[Tree[Term]]) -> Term:
+        tree: Tree[Term] = body[0]
+        terms: Sequence[Term] = [cast(Term, child) for child in tree.children]
+        target, arguments = terms[0], terms[1:]
+        return Apply(target=target, arguments=arguments)
+
+    @v_args(inline=True)
+    def immediate(self, _: Token, value: int) -> Term:
+        return Immediate(value=value)
+
+    @v_args(inline=True)
+    def primitive(self, operator: Token, left: Term, right: Term) -> Term:
+        return Primitive(operator=operator.value, left=left, right=right)
+
+    @v_args(inline=True)
+    def branch(self, _: Token, operator: Token, left: Term, right: Term, consequent: Term, otherwise: Term) -> Term:
+        return Branch(operator=operator.value, left=left, right=right, consequent=consequent, otherwise=otherwise)
+
+    @v_args(inline=True)
+    def allocate(self, _: Token, count: Nat) -> Term:
+        return Allocate(count=count)
+
+    @v_args(inline=True)
+    def load(self, _: Token, base: Term, index: Nat) -> Term:
+        return Load(base=base, index=index)
+
+    @v_args(inline=True)
+    def store(self, _: Token, base: Term, index: Nat, value: Term) -> Term:
+        return Store(base=base, index=index, value=value)
+
+    def begin(self, body: tuple[Token, Tree[Sequence[Term]]]) -> Term:
+        tree: Tree[Sequence[Term]] = body[1]
+        terms: Sequence[Term] = [cast(Term, child) for child in tree.children]
+        effects, value = terms[0:-1], terms[-1]
+        return Begin(effects=effects, value=value)
 
     def bindings(
         self,
